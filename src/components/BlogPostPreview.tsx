26b90086dede12fae66d31d7639fd2946da31540
David Amey <djamey87@gmail.com>
2024-06-26 08:47:12 +0100

    :lipstick: updating blog preview components

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -5,44 +5,53 @@ import { formatDate } from "date-fns";
 import Image from "next/image";
 import Link from "next/link";
 import { FunctionComponent } from "react";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { badgeVariants } from "@/components/ui/badge";
 
 export const BlogPostPreview: FunctionComponent<{
   post: GetPostsResult["posts"][0];
 }> = ({ post }) => {
   console.log("testing", JSON.stringify(post));
   return (
-    <Link href={`/blog/${post.slug}`}>
-      <Card className="overflow-hidden hover:ring-2">
-        <div className="aspect-[3/4] relative">
-          <Image
-            alt={post.title}
-            className="object-cover"
-            src={post.image || "/images/placeholder.webp"}
-            fill
-          />
-        </div>
-        <CardContent className="pb-0">
-          <div className="grid grid-cols-1 gap-2 md:col-span-2 my-2">
-            <h4 className="font-sans font-semibold tracking-tighter text-primary">
-              {post.title}
-            </h4>
-            <p className="prose text-sm lg:prose-lg tracking-tighter text-muted-foreground">
-              {formatDate(post.publishedAt || post.updatedAt, "dd MMMM yyyy")}
-            </p>
+    <div>
+      <Link href={`/blog/${post.slug}`}>
+        <Card className="overflow-hidden hover:ring-2">
+          <div className="aspect-[3/4] relative">
+            <Image
+              alt={post.title}
+              className="object-cover"
+              src={post.image || "/images/placeholder.webp"}
+              fill
+            />
           </div>
-        </CardContent>
-        {/* <CardFooter>
-          <div className="text-sm text-muted-foreground">
-            {post.tags.map((tag) => (
-              <div key={tag.id} className="mr-2 inline-block">
-                <Link href={`/tag/${tag.name}`}>#{tag.name}</Link>
-              </div>
-            ))}
-          </div>
+          <CardContent className="pb-0">
+            <div className="grid grid-cols-1 gap-2 md:col-span-2 my-2">
+              <p className="prose text-xs lg:prose-lg tracking-tighter text-muted-foreground">
+                {formatDate(post.publishedAt || post.updatedAt, "dd MMMM yyyy")}
+              </p>
+              <h4 className="font-sans font-semibold tracking-tighter text-primary">
+                {post.title}
+              </h4>
+            </div>
+          </CardContent>
+          {/* <CardFooter>
+          
         </CardFooter> */}
-      </Card>
-    </Link>
+        </Card>
+      </Link>
+      <div className="text-sm text-muted-foreground mt-4">
+        {post.tags.map((tag) => (
+          <div key={tag.id} className="mr-2 inline-block">
+            <Link
+              className={badgeVariants({ variant: "secondary" })}
+              href={`/tag/${tag.name}`}
+            >
+              #{tag.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
