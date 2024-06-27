7e5dc85ae1f841f592cb29987df4276bf7124731
David Amey <djamey87@gmail.com>
2024-06-27 16:11:34 +0100

    :lipstick: resolving social button styles

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -8,14 +8,14 @@ export const SocialLinks = ({ variant = "body" }: Props) => {
   return (
     <div className="flex flex-row mt-4">
       <ul className="float-left">
-        <li
-          className={`float-left hover:bg-accent ${
-            variant === "body" ? "pl-2 pr-4" : "px-3"
-          } py-2 rounded-lg shadow-md dark:ring-accent dark:ring-2`}
-        >
+        <li className="float-left">
           <a
             href="mailto:djamey87+hello@gmail.com?subject=Hello%20Dave"
-            className="flex items-center"
+            className={`flex items-center hover:bg-accent ${
+              variant === "body"
+                ? "bg-primary text-white hover:bg-primary/80 pl-2 pr-4"
+                : "px-3"
+            } py-2 rounded-lg shadow-md dark:ring-accent dark:ring-2`}
           >
             <Mail size="18" />
             {variant === "footer" ? null : (
@@ -23,13 +23,21 @@ export const SocialLinks = ({ variant = "body" }: Props) => {
             )}
           </a>
         </li>
-        <li className="float-left hover:bg-accent py-2 px-3 rounded-lg shadow-md ml-4 dark:ring-accent dark:ring-2">
-          <a href="https://www.linkedin.com/in/djamey87/" target="_blank">
+        <li className="float-left ml-4">
+          <a
+            className="block hover:bg-accent py-2 px-3 rounded-lg shadow-md dark:ring-accent dark:ring-2"
+            href="https://www.linkedin.com/in/djamey87/"
+            target="_blank"
+          >
             <Linkedin size="18" />
           </a>
         </li>
-        <li className="float-left ml-4 hover:bg-accent py-2 px-3 rounded-lg shadow-md dark:ring-accent dark:ring-2">
-          <a href="https://github.com/djamey87" target="blank">
+        <li className="float-left ml-4">
+          <a
+            className="block hover:bg-accent py-2 px-3 rounded-lg shadow-md dark:ring-accent dark:ring-2"
+            href="https://github.com/djamey87"
+            target="blank"
+          >
             <Github size="18" />
           </a>
         </li>
