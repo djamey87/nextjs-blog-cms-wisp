558187423624d1070fba338a4805624bb1480f21
David Amey <djamey87@gmail.com>
2024-06-27 12:03:27 +0100

    :wrench: adding vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Merriweather, Oxygen } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontSans = Oxygen({
   subsets: ["latin"],
@@ -58,6 +59,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
